bd29320323207576550720a419be9a5435d40728
Amit Sinha <amitsinha@Amits-MacBook-Pro.local>
2022-10-28 20:01:30 +0530

    Added maping for Nvim-tree

--- lua/zeal/remap.lua
+++ lua/zeal/remap.lua
@@ -1,3 +1,5 @@
+-- Remap nvim commands 
+-- Nnoremap 
 local nnoremap = require("zeal.keymap").nnoremap
 local inoremap = require("zeal.keymap").inoremap
 
@@ -6,10 +8,18 @@ nnoremap("<leader>pv", "<cmd>Ex<CR>")
 -- Nvim-Tree
 nnoremap("<leader>nt", "<cmd>NvimTreeToggle<CR>")
 nnoremap("<leader>nf", "<cmd>NvimTreeFocus<CR>")
+nnoremap("<leader>ns", "<cmd>NvimTreeFindFile<CR>")
+nnoremap("<leader>nr", "<cmd>NvimTreeRefresh<CR>")
 
 -- Telescope
 nnoremap("<leader>ff", "<cmd>Telescope find_files<CR>")
 nnoremap("<leader>fg", "<cmd>Telescope live_grep<CR>")
 nnoremap("<leader>fb", "<cmd>Telescope buffers<CR>")
 nnoremap("<leader>fh", "<cmd>Telescope help_tags<CR>")
+
+-- Vim WinCommand
+-- Move to file explorer
+nnoremap("<leader>h", "<cmd>wincmd h<CR>")
+-- Move to file
+nnoremap("<leader>l", "<cmd>wincmd l<CR>")
 
